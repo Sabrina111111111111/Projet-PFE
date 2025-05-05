8087a5f0f9eb64b9fc744032298c7137b7930354
Sabrina111111111111 <Mongiagrira@gmail.com>
2025-05-05 15:06:51 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API fonctionnelle !');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(Serveur démarré sur le port );
+});
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API fonctionnelle !');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
